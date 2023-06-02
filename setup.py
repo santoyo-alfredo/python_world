a601a9158edb225219e90a9df5e058d4289e9b81
Alfredo Santoyo <86384267+santoyo-alfredo@users.noreply.github.com>
2023-06-02 17:39:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,5 +17,13 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    install_requires=["numpy==1.22.1",
+        "matplotlib==3.6.2",
+        "pandas==1.4.4",
+        "plotly==5.10.0",
+        "pytest==7.2.0",
+        "scikit-learn==1.1.3",
+        "scipy==1.7.3",
+        "seaborn==0.12.1"]
 )
